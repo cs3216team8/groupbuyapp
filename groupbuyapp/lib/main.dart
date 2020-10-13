9fb4bf1d112df050d28b457204207d2106a02c0b
Yoke Kai Xin <kxyoke@gmail.com>
2020-10-14 00:07:46 +0800

    Add flutterfire dependencies.

--- groupbuyapp/lib/main.dart
+++ groupbuyapp/lib/main.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
 
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        //Check for errors
+        if (snapshot.hasError) {
+          print("firebase failed");
+          return TmpFix(); // SomethingWentWrong();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          print("firebase connected");
+          return TmpFix();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        print("firebase loading");
+        return TmpFix(); // Loading();
+      },
+    );
+  }
+}
+class TmpFix extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
