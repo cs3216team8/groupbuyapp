b99d07e3ccf3ebe06728b1dcab77fdfbd09b5c94
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-17 14:18:06 +0800

    Add GroupBuy model and dummy data

--- groupbuyapp/lib/models/GroupBuy.dart
+++ groupbuyapp/lib/models/GroupBuy.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/material.dart';
-
-// Import the firebase_core and cloud_firestore plugin
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'Buy.dart';
 
 class GroupBuy {
   final String storeName;
@@ -28,7 +25,27 @@ class GroupBuy {
       this.address
       );
 
-  GroupBuy getDummyData() {
-    return new GroupBuy(storeName, storeWebsite, storeLogo, currentAmount, targetAmount, endTimestamp, username, deposit, description, address)
+
+  static GroupBuy getDummyData() {
+    return new GroupBuy(
+        "Amazon.sg",
+        "https://www.amazon.sg/",
+        "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d825aa26de3150009a4616c%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D416%26cropY1%3D0%26cropY2%3D416",
+        0,
+        80,
+        1602865379,
+        "dawo",
+        0.8,
+        "Trusted and fast purchase",
+        "17 Dover Cres 130017"
+    );
+  }
+
+  static List<GroupBuy> getDummyDataAllGroupBuys() {
+    return [getDummyData(), getDummyData(), getDummyData()];
+  }
+
+  static List<Buy> getDummyDataBuysOfGroupBuy() {
+    return [Buy.getDummyData(), Buy.getDummyData(), Buy.getDummyData()];
   }
 }
