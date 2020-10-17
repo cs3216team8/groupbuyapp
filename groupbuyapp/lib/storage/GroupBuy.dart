6fe0ed1ad6510959a1d92ae3715e3162bbeaf270
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-17 18:43:37 +0800

    fix function to return list of widgets

--- groupbuyapp/lib/storage/GroupBuy.dart
+++ groupbuyapp/lib/storage/GroupBuy.dart
@@ -4,42 +4,38 @@ import 'package:groupbuyapp/models/GroupBuy.dart';
 import 'package:groupbuyapp/pages/component/grid_card_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class GetAllGroupBuys extends StatelessWidget {
-  GetAllGroupBuys();
 
-  @override
-  Widget build(BuildContext context) {
-    CollectionReference groupBuys = FirebaseFirestore.instance.collection(
-        'groupBuys');
-    return FutureBuilder<QuerySnapshot>(
-      future: groupBuys.get(),
-      builder:
-          (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text("Something went wrong");
-        }
+List<Widget> getAllGroupBuys(BuildContext context) {
+  CollectionReference groupBuys = FirebaseFirestore.instance.collection(
+      'groupBuys');
+  FutureBuilder<QuerySnapshot>(
+    future: groupBuys.get(),
+    builder:
+        (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+      if (snapshot.hasError) {
+        return Text("Something went wrong");
+      }
 
-        if (snapshot.connectionState == ConnectionState.done) {
-          snapshot.data.documents.map((DocumentSnapshot document) {
-            return new GroupBuyCard(
-                GroupBuy(
-                    document.data()['storeName'],
-                    document.data()['storeWebsite'],
-                    document.data()['storeLogo'],
-                    document.data()['currentAmount'],
-                    document.data()['targetAmount'],
-                    document.data()['endTimestamp'],
-                    document.data()['username'],
-                    document.data()['deposit'],
-                    document.data()['description'],
-                    document.data()['address']
-                )
-            );
-          }).toList();
-        }
+      if (snapshot.connectionState == ConnectionState.done) {
+        snapshot.data.documents.map((DocumentSnapshot document) {
+          return new GroupBuyCard(
+              GroupBuy(
+                  document.data()['storeName'],
+                  document.data()['storeWebsite'],
+                  document.data()['storeLogo'],
+                  document.data()['currentAmount'],
+                  document.data()['targetAmount'],
+                  document.data()['endTimestamp'],
+                  document.data()['username'],
+                  document.data()['deposit'],
+                  document.data()['description'],
+                  document.data()['address']
+              )
+          );
+        }).toList();
+      }
 
-        return Text("loading");
-        },
-    );
-  }
+      return Text("loading");
+      },
+  );
 }
