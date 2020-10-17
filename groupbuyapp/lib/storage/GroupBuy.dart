1a74f15f1733f7da9fa73499301c20e129abf3e1
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-17 16:54:03 +0800

    Add function to get list of widgets which will be displayed by home widget later

---
+++ groupbuyapp/lib/storage/GroupBuy.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'package:groupbuyapp/models/GroupBuy.dart';
+import 'package:groupbuyapp/pages/component/grid_card_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GetAllGroupBuys extends StatelessWidget {
+  GetAllGroupBuys();
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference groupBuys = FirebaseFirestore.instance.collection(
+        'groupBuys');
+    return FutureBuilder<QuerySnapshot>(
+      future: groupBuys.get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          snapshot.data.documents.map((DocumentSnapshot document) {
+            return new GroupBuyCard(
+                GroupBuy(
+                    document.data()['storeName'],
+                    document.data()['storeWebsite'],
+                    document.data()['storeLogo'],
+                    document.data()['currentAmount'],
+                    document.data()['targetAmount'],
+                    document.data()['endTimestamp'],
+                    document.data()['username'],
+                    document.data()['deposit'],
+                    document.data()['description'],
+                    document.data()['address']
+                )
+            );
+          }).toList();
+        }
+
+        return Text("loading");
+        },
+    );
+  }
+}
