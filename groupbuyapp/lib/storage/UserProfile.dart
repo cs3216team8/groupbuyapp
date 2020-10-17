a2289d849b9aedc95df2e056e1dc2a0948fdb5c2
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-17 18:56:25 +0800

    add connector to get the user profile widget

---
+++ groupbuyapp/lib/storage/UserProfile.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:groupbuyapp/models/UserProfile.dart';
+import 'package:groupbuyapp/pages/component/grid_card_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+Widget getUserProfile(BuildContext context, String userId) {
+  DocumentReference userProfile = FirebaseFirestore.instance
+      .collection('users')
+      .doc(userId);
+
+  FutureBuilder<DocumentSnapshot>(
+    future: userProfile.get(),
+    builder:
+        (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+      if (snapshot.hasError) {
+        return Text("Something went wrong");
+      }
+
+      if (snapshot.connectionState == ConnectionState.done) {
+        Map<String, dynamic> data = snapshot.data.data();
+        return UserProfileCard(
+            new UserProfile(
+                data['name'],
+                data['username'],
+                data['profilePicture'],
+                data['phoneNumber'],
+                data['email'],
+                data['addresses'],
+                data['groupBuyIds'],
+            )
+        )
+        }
+      }
+
+      return Text("loading");
+    },
+  );
+}
