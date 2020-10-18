96aa42d71d5605f7ebc7401fc9febf36fc71ffbd
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-19 01:47:04 +0800

    Fix function to ad dand list all group buys and return future objects

--- groupbuyapp/lib/storage/group_buy_storage.dart
+++ groupbuyapp/lib/storage/group_buy_storage.dart
@@ -11,6 +11,7 @@ Future<void> addGroupBuy(GroupBuy groupBuy) {
   CollectionReference groupBuys = FirebaseFirestore.instance.collection(
       'groupBuys');
   String groupBuyId = groupBuys.doc().id;
+
   return groupBuys.doc(groupBuyId).set({
     'storeName': groupBuy.storeName,
     'storeWebsite': groupBuy.storeWebsite,
@@ -27,12 +28,40 @@ Future<void> addGroupBuy(GroupBuy groupBuy) {
     .catchError((error) => print("Failed to add group buy: $error"));
 }
 
-FutureOr<List<GroupBuy>> getAllGroupBuys() {
+Stream<List<GroupBuy>> getAllGroupBuys() {
   CollectionReference groupBuys = FirebaseFirestore.instance.collection(
       'groupBuys');
-    new FutureOr<List<GroupBuy>>(
-      groupBuys.get().then((QuerySnapshot snapshot) {
-        return snapshot.docs.map((DocumentSnapshot document) {
+  return groupBuys.snapshots().map((snapshot) {
+    return snapshot.docs.map((document) {
+      return GroupBuy(
+          document.data()['storeName'],
+          document.data()['storeWebsite'],
+          document.data()['storeLogo'],
+          document.data()['currentAmount'].toDouble(),
+          document.data()['targetAmount'].toDouble(),
+          document.data()['endTimestamp'],
+          document.data()['username'],
+          document.data()['deposit'],
+          document.data()['description'],
+          document.data()['address']
+      );
+    }).toList();
+  });
+}
+
+/*
+Stream<GroupBuy> getAllGroupBuys() {
+  CollectionReference groupBuys = FirebaseFirestore.instance.collection(
+      'groupBuys');
+  Stream<QuerySnapshot> query = groupBuys.snapshots();
+  Stream<Future<GroupBuy>> result = query.map((QuerySnapshot snapshot) {
+    return snapshot.data.documents.map(
+
+    )
+    )
+  new Stream<GroupBuy>.fromFutures(
+
+.asyncMap((DocumentSnapshot document) {
           return new GroupBuy(
               document.data()['storeName'],
               document.data()['storeWebsite'],
@@ -47,8 +76,5 @@ FutureOr<List<GroupBuy>> getAllGroupBuys() {
           );
         }).toList();
       }
+*/
 
-      return Text("loading");
-      },
-  );
-}
