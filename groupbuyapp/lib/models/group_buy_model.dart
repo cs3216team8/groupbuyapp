90228046fa95d096a19cbf287a763e2b9673477e
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-19 01:44:56 +0800

    Add getter and setter function to GroupBuy

--- groupbuyapp/lib/models/group_buy_model.dart
+++ groupbuyapp/lib/models/group_buy_model.dart
@@ -1,12 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'Buy.dart';
 
 class GroupBuy {
   final String storeName;
   final String storeWebsite;
   final String storeLogo;
-  final double currentAmount;
-  final double targetAmount;
-  final double endTimestamp;
+  double currentAmount;
+  double targetAmount;
+  final Timestamp endTimestamp;
   final String username;
   final double deposit;
   final String description;
@@ -33,7 +35,7 @@ class GroupBuy {
         "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d825aa26de3150009a4616c%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D416%26cropY1%3D0%26cropY2%3D416",
         0,
         80,
-        1602865379,
+        Timestamp.fromDate(DateTime.now()),
         "dawo",
         0.8,
         "Trusted and fast purchase",
@@ -48,4 +50,34 @@ class GroupBuy {
   static List<Buy> getDummyDataBuysOfGroupBuy() {
     return [Buy.getDummyData(), Buy.getDummyData(), Buy.getDummyData()];
   }
+
+  double getTargetAmount() {
+    return this.targetAmount;
+  }
+
+  double getCurrentAmount() {
+    return this.currentAmount;
+  }
+
+  DateTime getTimeEnd() {
+    return this.endTimestamp.toDate();
+  }
+
+  void setTargetAmount(double amt) {
+    this.targetAmount = amt;
+  }
+
+  void setCurrentAmount(double amt) {
+    this.currentAmount = amt;
+  }
+
+  void addAmount(double amt) {
+    this.currentAmount += amt;
+  }
+
+  void removeAmount(double amt) {
+    assert(this.currentAmount - amt >= 0, "Error: attempting to remove more than current amount.");
+    this.currentAmount -= amt;
+  }
+
 }
