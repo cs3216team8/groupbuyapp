eba1f9bfab94889df99ed702128c4b4c9f5dabd2
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-20 03:03:02 +0800

    Change field name from username to user id in group buy

--- groupbuyapp/lib/models/group_buy_model.dart
+++ groupbuyapp/lib/models/group_buy_model.dart
@@ -1,27 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'Buy.dart';
+import 'buy_model.dart';
 
 class GroupBuy {
+  final String id;
   final String storeName;
   final String storeWebsite;
   final String storeLogo;
   double currentAmount;
   double targetAmount;
   final Timestamp endTimestamp;
-  final String username;
+  final String organiserId;
   final double deposit;
   final String description;
   final String address;
+  List<Buy> buys;
 
   GroupBuy(
+      this.id,
       this.storeName,
       this.storeWebsite,
       this.storeLogo,
       this.currentAmount,
       this.targetAmount,
       this.endTimestamp,
-      this.username,
+      this.organiserId,
       this.deposit,
       this.description,
       this.address
@@ -30,6 +33,7 @@ class GroupBuy {
 
   static GroupBuy getDummyData() {
     return new GroupBuy(
+        "asdasdasdasd",
         "amazon.sg",
         "https://www.amazon.sg/",
         "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d825aa26de3150009a4616c%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D416%26cropY1%3D0%26cropY2%3D416",
@@ -49,6 +53,18 @@ class GroupBuy {
 
   static List<Buy> getDummyDataBuysOfGroupBuy() {
     return [Buy.getDummyData(), Buy.getDummyData(), Buy.getDummyData()];
+  }
+
+  void addBuy(Buy buy) {
+    this.buys.add(buy);
+  }
+
+  void setBuys(List<Buy> buy) {
+    this.buys = buy;
+  }
+
+  List<Buy> getBuys() {
+    return this.buys;
   }
 
   double getTargetAmount() {
