5d51bc6be2c557e0131d0e23a32a5ad32d3b173c
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-10-20 03:02:19 +0800

    rename buy model, add necessary fields and functions

--- groupbuyapp/lib/models/buy_model.dart
+++ groupbuyapp/lib/models/buy_model.dart
@@ -1,12 +1,16 @@
 class Buy {
-  final String username;
+  final String id;
+  final String groupBuyId;
+  final String buyerId;
   final String itemLink;
   final double amount;
   final int quantity;
   final String comment;
 
   Buy(
-      this.username,
+      this.id,
+      this.groupBuyId,
+      this.buyerId,
       this.itemLink,
       this.amount,
       this.quantity,
@@ -15,7 +19,9 @@ class Buy {
 
   static Buy getDummyData() {
     return new Buy(
-        "dawo",
+        "sdfsdfsdf",
+        "sdfsdfsdf",
+        "o834798373",
         "https://www.amazon.sg/ASUS-VG27BQ-TUF-Gaming/dp/B07Z437T74?ref_=Oct_s9_apbd_omg_hd_bw_b71ZIJf&pf_rd_r=HNEDPN40Y917NSFMGQG6&pf_rd_p=364b9bf2-d790-5070-a722-fcf711fdcb6a&pf_rd_s=merchandised-search-10&pf_rd_t=BROWSE&pf_rd_i=6436118051",
         579,
         1,
