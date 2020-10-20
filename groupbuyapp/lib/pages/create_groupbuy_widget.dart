0bb977c6ddcdadd626bc1837e328afabfa0f90ab
dawo5010 <me@dawo.me>
2020-10-20 11:12:39 +0800

    Added basic create screen.

--- groupbuyapp/lib/pages/create_groupbuy_widget.dart
+++ groupbuyapp/lib/pages/create_groupbuy_widget.dart
@@ -3,8 +3,34 @@ import 'package:flutter/material.dart';
 class CreateGroupBuy extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Text(
-        'To be done'
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Text(
+              'Website'
+          ),
+          TextField(
+
+          ),
+          Text(
+            'Target amount'
+          ),
+          TextField(
+
+          ),
+          Text(
+            'Current Amount'
+          ),
+          TextField(
+
+          ),
+          RaisedButton(
+            child: Text('Create'),
+              onPressed: null
+          )
+        ]
+      )
     );
   }
 }
