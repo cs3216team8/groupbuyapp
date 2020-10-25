35669f1415bc0008645afe64347e203ddd27f47a
teikjun <teikjunhci@gmail.com>
2020-10-25 16:52:52 +0800

    remove isLiked

--- groupbuyapp/lib/models/message_model.dart
+++ groupbuyapp/lib/models/message_model.dart
@@ -2,17 +2,14 @@ import 'package:groupbuyapp/models/user_model.dart';
 
 class Message {
   final User sender;
-  final String
-  time; // Would usually be type DateTime or Firebase Timestamp in production apps
+  final String time; // Would usually be type DateTime or Firebase Timestamp in production apps
   final String text;
-  final bool isLiked;
   final bool unread;
 
   Message({
     this.sender,
     this.time,
     this.text,
-    this.isLiked,
     this.unread,
   });
 }
@@ -72,49 +69,42 @@ List<Message> chats = [
     sender: james,
     time: '5:30 PM',
     text: 'Hey, how\'s it going? What did you do today?',
-    isLiked: false,
     unread: true,
   ),
   Message(
     sender: olivia,
     time: '4:30 PM',
     text: 'Hey, how\'s it going? What did you do today?',
-    isLiked: false,
     unread: true,
   ),
   Message(
     sender: john,
     time: '3:30 PM',
     text: 'Hey, how\'s it going? What did you do today?',
-    isLiked: false,
     unread: false,
   ),
   Message(
     sender: sophia,
     time: '2:30 PM',
     text: 'Hey, how\'s it going? What did you do today?',
-    isLiked: false,
     unread: true,
   ),
   Message(
     sender: steven,
     time: '1:30 PM',
     text: 'Hey, how\'s it going? What did you do today?',
-    isLiked: false,
     unread: false,
   ),
   Message(
     sender: sam,
     time: '12:30 PM',
     text: 'Hey, how\'s it going? What did you do today?',
-    isLiked: false,
     unread: false,
   ),
   Message(
     sender: greg,
     time: '11:30 AM',
     text: 'Hey, how\'s it going? What did you do today?',
-    isLiked: false,
     unread: false,
   ),
 ];
@@ -125,42 +115,36 @@ List<Message> messages = [
     sender: james,
     time: '5:30 PM',
     text: 'Hey, how\'s it going? What did you do today?',
-    isLiked: true,
     unread: true,
   ),
   Message(
     sender: currentUser,
     time: '4:30 PM',
     text: 'Just walked my doge. She was super duper cute. The best pupper!!',
-    isLiked: false,
     unread: true,
   ),
   Message(
     sender: james,
     time: '3:45 PM',
     text: 'How\'s the doggo?',
-    isLiked: false,
     unread: true,
   ),
   Message(
     sender: james,
     time: '3:15 PM',
     text: 'All the food',
-    isLiked: true,
     unread: true,
   ),
   Message(
     sender: currentUser,
     time: '2:30 PM',
     text: 'Nice! What kind of food did you eat?',
-    isLiked: false,
     unread: true,
   ),
   Message(
     sender: james,
     time: '2:00 PM',
     text: 'I ate so much food today.',
-    isLiked: false,
     unread: true,
   ),
 ];
