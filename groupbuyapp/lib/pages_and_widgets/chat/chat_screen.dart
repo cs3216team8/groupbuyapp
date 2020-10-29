34e9741d374b87924a16df9fa23b8f9390e9344f
teikjun <teikjunhci@gmail.com>
2020-10-29 17:15:48 +0800

    add methods and refactor into storage

--- groupbuyapp/lib/pages_and_widgets/chat/chat_screen.dart
+++ groupbuyapp/lib/pages_and_widgets/chat/chat_screen.dart
@@ -7,6 +7,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:uuid/uuid.dart';
+import 'package:groupbuyapp/storage/chat_storage.dart';
 
 class ChatScreen extends StatefulWidget {
   final String username;
@@ -29,75 +30,12 @@ class _ChatScreenState extends State<ChatScreen> {
     super.initState();
   }
 
-  void onSend(ChatMessage message) {
-    var documentReference = FirebaseFirestore.instance
-        .collection('messages')
-        .doc(DateTime.now().millisecondsSinceEpoch.toString());
-
-    FirebaseFirestore.instance.runTransaction((transaction) async {
-      await transaction.set(
-        documentReference,
-        message.toJson(),
-      );
-    });
-  }
-
   void onSendMessage(ChatMessage message) {
-    var documentReference = FirebaseFirestore.instance
-        .collection('chatRooms')
-        .doc("testChatRoom")
-        .collection('messages')
-        .doc(DateTime.now().millisecondsSinceEpoch.toString());
-
-    FirebaseFirestore.instance.runTransaction((transaction) async {
-      await transaction.set(
-        documentReference,
-        message.toJson(),
-      );
-    });
+    ChatStorage().onSendMessage(message);
   }
-
 
   void uploadFile() async {
-    PickedFile pickedFile = await (new ImagePicker()).getImage(
-      source: ImageSource.gallery,
-      imageQuality: 80,
-      maxHeight: 400,
-      maxWidth: 400,
-    );
-    final File result = File(pickedFile.path);
-
-    if (result != null) {
-      String id = Uuid().v4().toString();
-
-      final StorageReference storageRef =
-          FirebaseStorage.instance.ref().child("chat_images/$id.jpg");
-
-      StorageUploadTask uploadTask = storageRef.putFile(
-        result,
-        StorageMetadata(
-          contentType: 'image/jpg',
-        ),
-      );
-      StorageTaskSnapshot download = await uploadTask.onComplete;
-
-      String url = await download.ref.getDownloadURL();
-
-      ChatMessage message = ChatMessage(text: "", user: user, image: url);
-
-      var documentReference = FirebaseFirestore.instance
-          .collection('chatRooms')
-          .doc("testChatRoom")
-          .collection('messages')
-          .doc(DateTime.now().millisecondsSinceEpoch.toString());
-
-      FirebaseFirestore.instance.runTransaction((transaction) async {
-        await transaction.set(
-          documentReference,
-          message.toJson(),
-        );
-      });
-    }
+    ChatStorage().uploadFileToStorage(user);
   }
 
   @override
