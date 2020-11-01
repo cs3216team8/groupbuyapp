9ae2f6625d40f06021279bc03f430f068e454184
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-11-01 22:10:42 +0800

    Set the empty default image for requests as piggybacker

--- groupbuyapp/lib/pages_and_widgets/groupbuy/request_as_piggybacker_default.dart
+++ groupbuyapp/lib/pages_and_widgets/groupbuy/request_as_piggybacker_default.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:groupbuyapp/utils/styles.dart';
 
 class RequestAsPiggyBackerDefaultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        SizedBox(height: 20.0,),
-        Text(
-          'You haven\'t requested yet!',
-          style: TextStyle(
-              fontSize:  30,
-              fontWeight: FontWeight.bold
-          ),
-          textAlign: TextAlign.center,
-        ),
-        RaisedButton(
-            textColor: Colors.white,
-            child: Text(
-                'Join this group buy!',
-                style: TextStyle(fontSize: 20)
-            )
-        ),
-      ],
+
+    return SingleChildScrollView(
+        padding: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20.0,),
+            Container(
+              child: SvgPicture.asset(
+                'assets/undraw_empty_xct9.svg',
+                height: 200,
+
+              ),
+              padding: EdgeInsets.all(10),
+            ),
+
+            Text(
+              'You haven\'t requested yet!',
+              style: Styles.emptyStyle,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        )
     );
   }
 }
