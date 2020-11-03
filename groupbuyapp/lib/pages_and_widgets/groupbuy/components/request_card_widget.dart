61768e14737cb34543e26ca8422af68305da7ffd
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-11-03 22:12:07 +0800

    Do not give a popup of the request details but migrate to a new screen of request details

--- groupbuyapp/lib/pages_and_widgets/groupbuy/components/request_card_widget.dart
+++ groupbuyapp/lib/pages_and_widgets/groupbuy/components/request_card_widget.dart
@@ -3,8 +3,11 @@ import 'package:groupbuyapp/models/group_buy_model.dart';
 import 'package:groupbuyapp/models/profile_model.dart';
 import 'package:groupbuyapp/models/request.dart';
 import 'package:groupbuyapp/pages_and_widgets/groupbuy/components/item_display_widget.dart';
+import 'package:groupbuyapp/pages_and_widgets/groupbuy/components/request_details_widget.dart';
 import 'package:groupbuyapp/storage/group_buy_storage.dart';
 import 'package:groupbuyapp/storage/profile_storage.dart';
+import 'package:groupbuyapp/utils/groupbuy/groupbuy_status.dart';
+import 'package:groupbuyapp/utils/navigators.dart';
 
 class RequestCard extends StatefulWidget {
   final GroupBuy groupBuy;
@@ -25,101 +28,7 @@ class RequestCard extends StatefulWidget {
 
 class _RequestCardState extends State<RequestCard> {
 
-  void onTapConfirm(BuildContext context) async {
-    await GroupBuyStorage.instance.confirmRequest(widget.groupBuy.id, widget.request);
-    Navigator.pop(context);
-    setState(() {
-      widget.request.status = RequestStatus.confirmed;
-    });
-  }
 
-  void onTapEdit(BuildContext context) {
-    print("on tap edit");
-    //TODO: @kx after @agnes completes join request form
-  }
-
-  Widget _showDetailedRequest(BuildContext context) {
-    return new AlertDialog(
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(height: 10,),
-          Divider(
-            color: Theme.of(context).dividerColor,
-            height: 5.5,
-          ),
-          SizedBox(height: 10,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                    'Items:',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                    )
-                ),
-              ),
-            ],
-          ),
-          Column(
-            children: widget.request.getItems().map((item) => ItemDisplay(item: item)).toList(),
-          ),
-          widget.isOrganiser? Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                 RaisedButton(
-                  color: Theme.of(context).accentColor,
-                  onPressed: () => onTapConfirm(context),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(Icons.add_business),
-                      Text("APPROVE"),
-                    ],
-                  ),
-                ),
-              ]
-          ): Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RaisedButton(
-                  child: Text("Edit"),
-                  onPressed: widget.request.isEditable() ? () => onTapEdit(context) : null,
-                ),
-              ]
-          ),
-        ],
-      )
-    );
-  }
-
-  void openDetailedRequest(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => _showDetailedRequest(context),
-    );
-  }
-
-  Color getStatusColor(RequestStatus status) {
-    switch (status) {
-      case RequestStatus.completed:
-        return Colors.green;
-      case RequestStatus.confirmed:
-        return Colors.blue;
-      case RequestStatus.pending:
-        return Colors.grey;
-      case RequestStatus.rejected:
-        return Colors.red;
-      case RequestStatus.accepted:
-        return Colors.greenAccent;
-      default:
-        return Colors.black;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -148,7 +57,7 @@ class _RequestCardState extends State<RequestCard> {
           alignment: Alignment.center,
           child: InkWell(
             splashColor: Theme.of(context).primaryColor.withAlpha(30),
-            onTap: () => openDetailedRequest(context),
+            onTap: () => segueToPage(context, RequestDetailsScreen(request: widget.request, groupBuy: widget.groupBuy, isOrganiser: widget.isOrganiser)),
             child: Column(
               children: <Widget>[
                 FutureBuilder(
