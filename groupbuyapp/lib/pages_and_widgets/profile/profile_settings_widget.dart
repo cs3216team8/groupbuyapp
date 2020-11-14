0180f2aaaf4ff4614eb1760a5f577c272a4ff99a
dawo5010 <me@dawo.me>
2020-11-14 16:13:31 +0800

    Fixed logout issues.

--- groupbuyapp/lib/pages_and_widgets/profile/profile_settings_widget.dart
+++ groupbuyapp/lib/pages_and_widgets/profile/profile_settings_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:groupbuyapp/models/profile_model.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -108,8 +109,7 @@ class _ProfileSettingsScreenState extends State<ProfileSettingsScreen> {
               await FirebaseAuth.instance.signOut();
               await GoogleSignIn().signOut();
               await FacebookLogin().logOut();
-              Navigator.pop(context);
-              segueWithoutBack(context, PiggyBuyApp());
+              Phoenix.rebirth(context);
             },
             textColor: Theme.of(context).primaryColor,
             child: Text(
