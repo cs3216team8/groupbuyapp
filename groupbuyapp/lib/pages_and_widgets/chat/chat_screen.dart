7bd6ca5648efea27ad7e487becbee19167b966b9
teikjun <teikjunhci@gmail.com>
2020-11-14 14:31:53 +0800

    refactoring

--- groupbuyapp/lib/pages_and_widgets/chat/chat_screen.dart
+++ groupbuyapp/lib/pages_and_widgets/chat/chat_screen.dart
@@ -93,10 +93,10 @@ class _ChatScreenState extends State<ChatScreen> {
               ],
               timeFormat: DateFormat('HH:mm'),
               dateFormat: DateFormat('MMM dd'),
-              messageTextBuilder : (String string, [ChatMessage]) {
+              messageTextBuilder: (String string, [ChatMessage]) {
                 return Text(
-                    string,
-                    style: Styles.chatMessageStyle,
+                  string,
+                  style: Styles.chatMessageStyle,
                 );
               },
               messageTimeBuilder: (String string, [ChatMessage]) {
