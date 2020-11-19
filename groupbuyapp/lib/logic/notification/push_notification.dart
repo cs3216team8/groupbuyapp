4224760fc353b52f76a0a3715a6b5b03bf309b05
teikjun <teikjunhci@gmail.com>
2020-11-19 16:40:24 +0800

    add scaffold for push notification

---
+++ groupbuyapp/lib/logic/notification/push_notification.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotification {
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  Future initialise() async {
+    if (Platform.isIOS) {
+      _fcm.requestNotificationPermissions(IosNotificationSettings());
+    }
+
+    _fcm.configure(
+      // called when app is in the foreground and push notification received
+      onMessage: (Map<String, dynamic> message) async {
+        print('onMessage: $message');
+      },
+
+      // called when app is closed completely and it's opened from push notification
+      onLaunch: (Map<String, dynamic> message) async {
+        print('onLaunch: $message');
+      },
+
+      // called when app is in the background and it's opened from push notification
+      onResume: (Map<String, dynamic> message) async {
+        print('onResume: $message');
+      },
+
+    );
+
+
+
+  }
+
+}
