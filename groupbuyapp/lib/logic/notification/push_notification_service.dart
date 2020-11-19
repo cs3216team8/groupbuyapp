0e81dab104b2833f6fc74326247491d2788b0042
teikjun <teikjunhci@gmail.com>
2020-11-19 19:05:39 +0800

    add navigate function to notification

--- groupbuyapp/lib/logic/notification/push_notification_service.dart
+++ groupbuyapp/lib/logic/notification/push_notification_service.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:groupbuyapp/utils/navigators.dart';
 
 class PushNotificationService {
   final FirebaseMessaging _fcm = FirebaseMessaging();
@@ -14,19 +15,30 @@ class PushNotificationService {
       // called when app is in the foreground and push notification received
       onMessage: (Map<String, dynamic> message) async {
         print('onMessage: $message');
+        _serialiseAndNavigate(message);
       },
 
       // called when app is closed completely and it's opened from push notification
       onLaunch: (Map<String, dynamic> message) async {
         print('onLaunch: $message');
+        _serialiseAndNavigate(message);
       },
 
       // called when app is in the background and it's opened from push notification
       onResume: (Map<String, dynamic> message) async {
         print('onResume: $message');
+        _serialiseAndNavigate(message);
       },
     );
   }
 
-  void _serialiseAndNavigate(Map<String, dynamic> message) {}
+  void _serialiseAndNavigate(Map<String, dynamic> message) {
+    var notificationData = message['data'];
+    var view = notificationData['view'];
+
+    if (view != null) {
+      if (view == 'home') {
+      }
+    }
+  }
 }
