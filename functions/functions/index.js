ba35ec023b124f71afd6f7dc0acb29805b7a3319
teikjun <teikjunhci@gmail.com>
2020-11-22 04:12:21 +0800

    add cloud function for request updates

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -8,28 +8,28 @@ const fcm = admin.messaging();
 
 exports.sendNotification = functions.firestore
     .document("chatRooms/{chatRoomId}/messages/{messageId}")
-    .onCreate(async snapshot => {
-        const message = snapshot.data();
-        const senderUid = message.user.uid;
-        const senderName = await db.collection("users").doc(senderUid).get().name;
-        const cid = context.params.chatRoomId;
-        const receiverUid = cid.replaceAll(senderUid, "").replaceAll("_", "");
+    .onCreate(async (snapshot, context) => {
+//        const message = snapshot.data();
+//        const senderUid = message.user.uid;
+//        const senderName = await db.collection("users").doc(senderUid).get().data().name;
+//        const cid = context.params.chatRoomId;
+//        const receiverUid = cid.replaceAll(senderUid, "").replaceAll("_", "");
         const querySnapshot = await db
             .collection('users')
-            .doc(receiverUid)
+            .doc('7RqTgkmFo9g1KbKdqdahCR6S2Th1') // change to receiverUid
             .collection('tokens')
             .get();
         const tokens = querySnapshot.docs.map(snap => snap.id);
         const payload = {
             notification: {
-                title: 'Chat Message from ' + senderName,
+                title: 'Chat Message from ', // add senderName
                 body: message.text,
             },
             data: {
                 'click_action': 'FLUTTER_NOTIFICATION_CLICK',
                 view: 'chat',
                 chatRoomId: 'ezDEidkJFbbLZN2TFI2fgJx8H9r1_7RqTgkmFo9g1KbKdqdahCR6S2Th1',
-                username: senderName,
+                username: 'senderName', // replace with senderName
             }
         };
 
@@ -39,11 +39,44 @@ exports.sendNotification = functions.firestore
 
 //exports.sendGroupBuyNotification = functions.firestore
 //    .document("groupBuys/{groupBuyId}")
-//    .onUpdate
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+//    .onUpdate(async (snapshot, context) => {
 //
+//    });
+
+//
+//exports.sendRequestNotification = functions.firestore
+//    .document("groupBuys/{requestId}")
+//    .onUpdate(async (snapshot, context) => {
+//        const request = snapshot.data();
+//        const requestorId = request.requestorId;
+//        const querySnapshot = await db
+//                    .collection('users')
+//                    .doc(requestorId) // change to receiverUid
+//                    .collection('tokens')
+//                    .get();
+//
+//        const payload = {
+//            notification: {
+//                title: 'Updates to your request", // add senderName
+//                body: 'Your request has been' + request.status,
+//            },
+//            data: {
+//                'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+//                view: 'request',
+//            }
+//        };
+//
+//        return fcm.sendToDevice(tokens, payload);
+//    });
+
+
+
+
+
+
+ // Create and Deploy Your First Cloud Functions
+ // https://firebase.google.com/docs/functions/write-firebase-functions
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
