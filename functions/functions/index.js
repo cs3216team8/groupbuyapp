4845299b42c75906809dee6c83e5310b9a341337
teikjun <teikjunhci@gmail.com>
2020-11-21 16:54:27 +0800

    add cloud functions

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,12 +1,31 @@
 const functions = require('firebase-functions');
 
-import * as admin from 'firebase-admin';
+const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
 const fcm = admin.messaging();
 
-export const sendToTopic
+exports.sendNotification = functions.firestore
+    .document("chatRooms/{chatRoomId}/messages/{messageId}")
+    .onCreate(async snapshot => {
+        const message = snapshot.data();
+        const querySnapshot = await db
+            .collection('users')
+            .doc('7RqTgkmFo9g1KbKdqdahCR6S2Th1')
+            .collection('tokens')
+            .get();
+        const tokens = querySnapshot.docs.map(snap => snap.id);
+        const payload = {
+            notification: {
+                title: 'New Message',
+                body: 'you got a message',
+                clickAction 'FLUTTER_NOTIFICATION_CLICK'
+            }
+        };
+
+        return fcm.sendToDevice(tokens, payload);
+    });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
