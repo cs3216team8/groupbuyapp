b805562633bc644c9c40ce05161f8dc61d364699
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-11-22 17:50:23 +0800

    Add reviewer user id to review object

--- groupbuyapp/lib/models/review_model.dart
+++ groupbuyapp/lib/models/review_model.dart
@@ -1,11 +1,13 @@
 class Review {
   final String revieweeUserId;
+  final String reviewerUserId;
   final double rating;
   final String review;
   final DateTime dateTime;
 
   Review (
       this.revieweeUserId,
+      this.reviewerUserId,
       this.rating,
       this.review,
       this.dateTime,
@@ -15,7 +17,8 @@ class Review {
   static Review getDummyData() {
     return new Review (
         "abcd1234",
-        4.7,
+      "abcd5678",
+      4.7,
         "The seller is very friendly and is on time",
         DateTime.now(),
     );
